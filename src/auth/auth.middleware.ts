9597799161e4f88ebc471289f16028b2da2a3d0d
alexandre-kakal-akarah <alexandre.kakal@akarah.com>
2025-01-24 10:20:41 +0100

    feat: SA-9 auth middleware

---
+++ src/auth/auth.middleware.ts
@@ -0,0 +1,34 @@
+import {
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request, Response, NextFunction } from 'express';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    const accessToken = req.cookies?.accessToken;
+    const refreshToken = req.cookies?.refreshToken;
+
+    if (!accessToken || !refreshToken) {
+      throw new UnauthorizedException('Access and refresh tokens are missing');
+    }
+
+    const payload = this.jwtService.verify(accessToken, {
+      secret: this.configService.get<string>('JWT_SECRET'),
+    });
+    if (!payload) {
+      throw new UnauthorizedException('Invalid token');
+    }
+    req.user = payload;
+    return next();
+  }
+}
